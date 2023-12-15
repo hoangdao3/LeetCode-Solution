99811ee452544df2b76ebb11fb4f0f7174232769
Dao Nguyen Huy Hoang <129637429+hoangdao3@users.noreply.github.com>
2023-12-15 09:37:57 +0700

    Add files via upload

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,25 @@
+/*
+    Climbing stairs, either 1 or 2 steps, distinct ways to reach top
+    Ex. n = 2 -> 2 (1 + 1, 2), n = 3 -> 3 (1 + 1 + 1, 1 + 2, 2 + 1)
+
+    Recursion w/ memoization -> DP, why DP? Optimal substructure
+    Recurrence relation: dp[i] = dp[i - 1] + dp[i - 2]
+    Reach ith step in 2 ways: 1) 1 step from i-1, 2) 2 steps from i-2
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+
+    }
+};
